9cd1683f6b0feab50f9c9ce50f05651a041ce0a8
vuasanban <vuasanbanx@gmail.com>
2022-04-08 12:00:27 +0700

    [UPDATE] forgot password send email

---
+++ utils/sendEmail.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendEmail = async (options) => {
+  // let testAccount = await nodemailer.createTestAccount();
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    // secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_USERNAME, // generated ethereal user
+      pass: process.env.SMTP_PASSWORD, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.message, // plain text body
+    // html: "<b>Hello world?</b>", // html body
+  };
+
+  const info = await transporter.sendMail(message);
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+};
+
+// main().catch(console.error);
+module.exports = sendEmail;
